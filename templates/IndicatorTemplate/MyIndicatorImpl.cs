b454442f1c9c6b2e8089c007cf7cc8a0478303b4
vntradesoft <vntradesoft@gmail.com>
2026-07-08 13:58:30 +0700

    docs(sdk): split indicator-plugin.md into indicator subfolder

--- templates/IndicatorTemplate/MyIndicatorImpl.cs
+++ templates/IndicatorTemplate/MyIndicatorImpl.cs
@@ -1,19 +1,53 @@
-using Pt.Okx.Sdk.Indicators;
+using System;
+using System.Collections.Generic;
+using Pt.Okx.Sdk.Indicators.Base;
+using Pt.Okx.Sdk.Indicators.Models;
+using Pt.Okx.Sdk.Indicators.Services;
 
 namespace MyCompany.MyIndicator
 {
-    public class MyIndicatorImpl : IIndicatorCalculator
+    public class MyIndicatorImpl : CalcIndBase
     {
-        public bool IsReady => true;
+        private IIndicatorBuffer _mainBuffer;
 
-        public void Calculate(decimal input)
+        public MyIndicatorImpl(IIndicatorFactory factory, IIndicatorManager manager, IndicatorConfig config, Action<IndicatorProperty>? propertyOptions = null)
+            : base(factory, manager, config, propertyOptions)
         {
-            // Core indicator calculation logic
+            // Set any parameters
+            // int period = GetParameter<int>("Period");
         }
 
-        public void Reset()
+        protected override IndicatorProperty CreateDefaultProperty()
         {
-            // Reset internal state
+            return new IndicatorProperty(Identity.IndicatorType, 1)
+            {
+                ShortName = "MyIndicator",
+                Precision = 2,
+                Levels = new List<double>(),
+                Colors = new List<System.Drawing.Color> { System.Drawing.Color.Blue }
+            };
+        }
+
+        public override bool OnInit()
+        {
+            if (!base.OnInit())
+                return false;
+
+            _mainBuffer = _manager.CreateBuffer(this, 0);
+            return true;
+        }
+
+        public override int OnCalculate(in int ratesTotal, in int prevCalculated, in int begin, in double[] prices)
+        {
+            int start = prevCalculated == 0 ? begin : prevCalculated - 1;
+
+            for (int i = start; i < ratesTotal; i++)
+            {
+                // Simple pass-through example
+                _mainBuffer.Set(i, prices[i]);
+            }
+
+            return ratesTotal;
         }
     }
 }
